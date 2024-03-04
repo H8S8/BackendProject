14997bb36a7266a6b91c0245a0c706c5a764d9b6
Hannah Riley <hannah.847@btinternet.com>
2024-03-04 14:47:03 +0000

    create project files with spring boot

---
+++ BackendProject/src/main/java/com/example/BackendProject/SupermarketInventoryManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketInventoryManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketInventoryManagmentApplication.class, args);
+	}
+
+}
---
+++ BackendProject/src/test/java/com/example/BackendProject/SupermarketInventoryManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketInventoryManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
