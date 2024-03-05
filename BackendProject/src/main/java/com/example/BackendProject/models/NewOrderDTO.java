c59f50491ffcbcb2772b5d3f3a6e89aca4f2079c
Hannah Riley <hannah.847@btinternet.com>
2024-03-05 12:34:12 +0000

    add constructors and getter and setter to newOrderDTO

--- BackendProject/src/main/java/com/example/BackendProject/models/NewOrderDTO.java
+++ BackendProject/src/main/java/com/example/BackendProject/models/NewOrderDTO.java
@@ -4,4 +4,18 @@ public class NewOrderDTO {
 
     private Long supermarketId;
 
+    public NewOrderDTO(Long supermarketId) {
+        this.supermarketId = supermarketId;
+    }
+
+    public NewOrderDTO() {
+    }
+
+    public Long getSupermarketId() {
+        return supermarketId;
+    }
+
+    public void setSupermarketId(Long supermarketId) {
+        this.supermarketId = supermarketId;
+    }
 }
