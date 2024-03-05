fe5a127ff5940b23d2aaa4c080be143a2044da9f
BirindarK <birindar@Birindars-MacBook-Air.local>
2024-03-05 10:16:28 +0000

    Ordered items model and repository completed

--- BackendProject/src/main/java/com/example/BackendProject/models/OrderedItem.java
+++ BackendProject/src/main/java/com/example/BackendProject/models/OrderedItem.java
@@ -1,4 +1,69 @@
 package com.example.BackendProject.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ordered_items")
 public class OrderedItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonIgnoreProperties ({"orderedItems"})
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @JsonIgnoreProperties ({"orderedItems"})
+    @ManyToOne
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+
+    @Column
+    private int orderQuantity;
+
+    public OrderedItem(Order order, Stock stock, int orderQuantity) {
+        this.order = order;
+        this.stock = stock;
+        this.orderQuantity = orderQuantity;
+    }
+
+    public OrderedItem() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public int getOrderQuantity() {
+        return orderQuantity;
+    }
+
+    public void setOrderQuantity(int orderQuantity) {
+        this.orderQuantity = orderQuantity;
+    }
+
+    //@Transactional Method to remove stock in Service Class - tbd
 }
--- BackendProject/src/main/java/com/example/BackendProject/repositories/OrderedItemRepository.java
+++ BackendProject/src/main/java/com/example/BackendProject/repositories/OrderedItemRepository.java
@@ -1,4 +1,10 @@
 package com.example.BackendProject.repositories;
 
-public interface OrderedItemRepository {
+import com.example.BackendProject.models.OrderedItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderedItemRepository extends JpaRepository <OrderedItem, Long> {
+    
 }
