5452b9fb977a879612468fa2893e2ec9608a2518
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-03-05 11:24:14 +0000

    ordereditem controller service, create and update tbd

--- BackendProject/src/main/java/com/example/BackendProject/controllers/OrderedItemController.java
+++ BackendProject/src/main/java/com/example/BackendProject/controllers/OrderedItemController.java
@@ -1,4 +1,50 @@
 package com.example.BackendProject.controllers;
 
+import com.example.BackendProject.models.OrderedItem;
+import com.example.BackendProject.services.OrderService;
+import com.example.BackendProject.services.OrderedItemService;
+import com.example.BackendProject.services.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("ordered-items")
 public class OrderedItemController {
+    @Autowired
+    OrderedItemService orderedItemService;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    StockService stockService;
+
+    @GetMapping
+    public ResponseEntity<List<OrderedItem>> getAllOrderedItems(){
+        return new ResponseEntity<>(orderedItemService.findAllOrderedItems(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OrderedItem> getOrderedItem(@PathVariable Long id){
+        Optional<OrderedItem> orderedItemFound = orderedItemService.findOrderedItem(id);
+        if (orderedItemFound.isPresent()) {
+            return new ResponseEntity<>(orderedItemFound.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+   // @PostMapping
+    //public ResponseEntity<OrderedItem> postOrderedItem(@RequestBody)
+
+    //@PatchMapping
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteOrderedItem(@PathVariable Long id){
+        orderedItemService.deleteOrderedItem(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
 }
--- BackendProject/src/main/java/com/example/BackendProject/services/OrderedItemService.java
+++ BackendProject/src/main/java/com/example/BackendProject/services/OrderedItemService.java
@@ -1,4 +1,31 @@
 package com.example.BackendProject.services;
 
+import com.example.BackendProject.models.OrderedItem;
+import com.example.BackendProject.models.Stock;
+import com.example.BackendProject.repositories.OrderedItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public class OrderedItemService {
+    @Autowired
+    OrderedItemRepository orderedItemRepository;
+    @Autowired
+    StockService stockService;
+    @Autowired
+    OrderService orderService;
+
+    public List<OrderedItem> findAllOrderedItems() {
+        return orderedItemRepository.findAll();
+    }
+
+    public Optional<OrderedItem> findOrderedItem(Long id) {
+        return orderedItemRepository.findById(id);
+    }
+
+    public void deleteOrderedItem(Long id) {
+        orderedItemRepository.deleteById(id);
+    }
 }
