cb8c351437b9797733db5e5db09e5131dab4d0db
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-03-05 10:31:29 +0000

    Added extension classes relating to Orders

--- BackendProject/src/main/java/com/example/BackendProject/models/OrderStatus.java
+++ BackendProject/src/main/java/com/example/BackendProject/models/OrderStatus.java
@@ -1,4 +1,16 @@
 package com.example.BackendProject.models;
 
 public enum OrderStatus {
+
+    PENDING("Pending"),
+    IN_PROGRESS("In Progress"),
+    OUT_FOR_DELIVERY("Out for delivery"),
+    COMPLETED("Completed");
+
+    final String displayStatus;
+
+    OrderStatus(String displayStatus) {
+        this.displayStatus = displayStatus;
+    }
+
 }
