4e2bd66627e75ffdb385cdadb378e4f1d00bd208
Hannah Riley <hannah.847@btinternet.com>
2024-03-05 11:31:54 +0000

    complete index and show endpoints for order model

--- BackendProject/src/main/java/com/example/BackendProject/controllers/OrderController.java
+++ BackendProject/src/main/java/com/example/BackendProject/controllers/OrderController.java
@@ -1,4 +1,39 @@
 package com.example.BackendProject.controllers;
 
+import com.example.BackendProject.models.Order;
+import com.example.BackendProject.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/orders")
 public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders(){
+        List<Order> orders = orderService.getAllOrders();
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable long id){
+        Optional<Order> order = orderService.getOrderById(id);
+        if(order.isPresent()){
+            return new ResponseEntity<>(order.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
 }
--- BackendProject/src/main/java/com/example/BackendProject/services/OrderService.java
+++ BackendProject/src/main/java/com/example/BackendProject/services/OrderService.java
@@ -1,4 +1,24 @@
 package com.example.BackendProject.services;
 
+import com.example.BackendProject.models.Order;
+import com.example.BackendProject.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public List<Order> getAllOrders(){
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getOrderById(long id){
+        return orderRepository.findById(id);
+    }
 }
